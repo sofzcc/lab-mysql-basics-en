ff010161621924733ac98a018e275353cabc6a4b
sofzcc <100369299+sofzcc@users.noreply.github.com>
2024-05-24 16:49:08 +0200

    Fixed result column names

--- files_for_lab/solutions.sql
+++ files_for_lab/solutions.sql
@@ -1,8 +1,8 @@
 -- Query 1
 -- Get the id values of the first 5 clients from district_id with a value equals to 1.
 
-SELECT client.client_id
-FROM bank.client
+SELECT client_id
+FROM client
 WHERE district_id = 1
 LIMIT 5;
 
@@ -10,8 +10,8 @@ LIMIT 5;
 -- Query 2
 -- In the client table, get an id value of the last client where the district_id equals to 72
 
-SELECT MAX(client.client_id)
-FROM bank.client
+SELECT MAX(client_id)
+FROM client
 WHERE district_id = 72;
 
 
@@ -99,7 +99,7 @@ ORDER BY date DESC;
 -- Query 13
 -- In the client table, of all districts with a district_id lower than 10, how many clients are from each district_id? Show the results sorted by the district_id in ascending order.
 
-SELECT COUNT(client_id)
+SELECT district_id "DISTRICT_ID", COUNT(client_id) "TOTAL CLIENTS"
 FROM client
 WHERE district_id < 10
 GROUP BY district_id
@@ -108,7 +108,7 @@ ORDER BY district_id ASC;
 -- Query 14
 -- In the card table, how many cards exist for each type? Rank the result starting with the most frequent type.
 
-SELECT type, COUNT(card_id)
+SELECT type, COUNT(card_id) "TOTAL CARDS"
 FROM card
 GROUP BY type
 ORDER BY COUNT(card_id) DESC;
@@ -116,16 +116,16 @@ ORDER BY COUNT(card_id) DESC;
 -- Query 15
 -- Using the loan table, print the top 10 account_ids based on the sum of all of their loan amounts.
 
-SELECT account_id, SUM(amount) AS total_amount
+SELECT account_id, SUM(amount) "TOTAL AMOUNT"
 FROM loan
 GROUP BY account_id
-ORDER BY total_amount DESC
+ORDER BY SUM(amount) DESC
 LIMIT 10;
 
 -- Query 16
 -- In the loan table, retrieve the number of loans issued for each day, before (excl) 930907, ordered by date in descending order.
 
-SELECT date, COUNT(loan_id)
+SELECT date, COUNT(loan_id) "TOTAL LOANS"
 FROM loan
 WHERE date < 930907
 GROUP BY date
@@ -134,16 +134,16 @@ ORDER BY date DESC;
 -- Query 17
 
 
-SELECT date, duration, COUNT(loan_id)
+SELECT date, duration, COUNT(loan_id) "TOTAL LOANS"
 FROM loan
-WHERE date <= 971231 AND date >= 971201
+WHERE date BETWEEN 971201 AND 971231
 GROUP BY date, duration
 ORDER BY date, duration ASC;
 
 -- Query 18
 -- In the trans table, for account_id 396, sum the amount of transactions for each type (VYDAJ = Outgoing, PRIJEM = Incoming). Your output should have the account_id, the type and the sum of amount, named as total_amount. Sort alphabetically by type.
 
-SELECT account_id, type, SUM(amount) AS total_amount
+SELECT account_id, type, SUM(amount) "TOTAL AMOUNT" 
 FROM trans
 WHERE account_id = 396
 GROUP BY account_id, type
